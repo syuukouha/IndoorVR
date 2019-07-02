5669b2d7ef5d25bbe819a56e653d97a0f2aa4d87
ZhouHongBo <252747500@qq.com>
2019-07-02 15:02:18 +0800

    Ver.1.0

---
+++ Assets/Scripts/IndoorInteraction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndoorInteraction : MonoBehaviour
+{
+    public GameObject[] Floors;
+
+    public void ChangeFloorMaterial(GameObject gameObject)
+    {
+        Material newMaterial = gameObject.GetComponent<MeshRenderer>().material;
+
+        for (int i = 0; i < Floors.Length; i++)
+        {
+            Floors[i].GetComponent<MeshRenderer>().material.CopyPropertiesFromMaterial(newMaterial);
+        }
+    }
+}
