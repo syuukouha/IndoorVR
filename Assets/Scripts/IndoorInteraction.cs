d90a3db38dff2fc1bb6d996aa2f6af88614b2f0e
ZhouHongBo <252747500@qq.com>
2019-07-19 15:45:31 +0800

    增加沙发交互

--- Assets/Scripts/IndoorInteraction.cs
+++ Assets/Scripts/IndoorInteraction.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class IndoorInteraction : MonoBehaviour
 {
-    public GameObject[] Floors;
+    public GameObject[] Floor;
+    public GameObject[] Sofa;
+    public GameObject[] Couch1;
+    public GameObject[] Couch2;
+
 
     public void ChangeFloorMaterial(GameObject gameObject)
     {
         Material newMaterial = gameObject.GetComponent<MeshRenderer>().material;
 
-        for (int i = 0; i < Floors.Length; i++)
+        for (int i = 0; i < Floor.Length; i++)
         {
-            Floors[i].GetComponent<MeshRenderer>().material.CopyPropertiesFromMaterial(newMaterial);
+            Floor[i].GetComponent<MeshRenderer>().material.CopyPropertiesFromMaterial(newMaterial);
+        }
+    }
+    public void ChangeSofa(string name)
+    {
+        switch (name)
+        {
+            case "Sofa":
+                for (int i = 0; i < Sofa.Length; i++)
+                {
+                    Sofa[i].SetActive(true);
+                    Couch1[i].SetActive(false);
+                    Couch2[i].SetActive(false);
+                }
+                break;
+            case "Couch1":
+                for (int i = 0; i < Sofa.Length; i++)
+                {
+                    Sofa[i].SetActive(false);
+                    Couch1[i].SetActive(true);
+                    Couch2[i].SetActive(false);
+                }
+                break;
+            case "Couch2":
+                for (int i = 0; i < Sofa.Length; i++)
+                {
+                    Sofa[i].SetActive(false);
+                    Couch1[i].SetActive(false);
+                    Couch2[i].SetActive(true);
+                }
+                break;
+            default:
+                for (int i = 0; i < Sofa.Length; i++)
+                {
+                    Sofa[i].SetActive(true);
+                    Couch1[i].SetActive(false);
+                    Couch2[i].SetActive(false);
+                }
+                break;
         }
     }
 }
